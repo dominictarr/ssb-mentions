b4bcd3df93e1498eeb74cf22b94767d03f5dd739
cel <cel@f/6sQ6d2CMxRUhLpspgGIulDxDCwYD7DzFzPNr7u5AU=.ed25519>
2017-02-12 22:34:07 -0800

    Simplify tests

--- test/mentions.js
+++ test/mentions.js
@@ -1,46 +1,58 @@
 var test = require('tape')
 var mentions = require('../')
 
-var id = {
-  feed: '@3HO6R2i60XNR3h6XCHAWCdt1k9Dwy+gaa2rVs6LzZ6Y=.ed25519',
-  msg: '%A2LvseOYKDXyuSGlXl3Sz0F5j2khVCN6JTf8ORD/tM8=.sha256',
-  blob: '&9SSTQys34p9f4zqjxvRwENjFX0JapgtesRey7+fxK14=.sha256'
-}
-id.all = [id.feed, id.msg, id.blob].join(' ')
-
-var text = {
-  feed: '[@feed](' + id.feed + ')',
-  msg: '[a msg](' + id.msg + ')',
-  blob: '[a blob](' + id.blob + ')'
-}
-text.all = [text.feed, text.msg, text.blob].join(' ')
-
-var linkMention = {
-  feed: [ { link: id.feed, name: 'feed' } ],
-  msg: [ { link: id.msg, name: 'a msg' } ],
-  blob: [ { link: id.blob, name: 'a blob' } ]
-}
-linkMention.all = linkMention.feed.concat(linkMention.msg, linkMention.blob)
-
 test('mentions in links are detected', function (t) {
-  t.deepEquals(mentions(text.feed), linkMention.feed, 'feed link')
-  t.deepEquals(mentions(text.msg), linkMention.msg, 'msg link')
-  t.deepEquals(mentions(text.blob), linkMention.blob, 'blob link')
-  t.deepEquals(mentions(text.all), linkMention.all, 'all links')
+  t.deepEquals(mentions(
+    '[@feed](@3HO6R2i60XNR3h6XCHAWCdt1k9Dwy+gaa2rVs6LzZ6Y=.ed25519)'), [
+      {
+        link: '@3HO6R2i60XNR3h6XCHAWCdt1k9Dwy+gaa2rVs6LzZ6Y=.ed25519',
+        name: 'feed',
+      }
+    ], 'feed link')
+
+  t.deepEquals(mentions(
+    '[a msg](%A2LvseOYKDXyuSGlXl3Sz0F5j2khVCN6JTf8ORD/tM8=.sha256)'), [
+      {
+        link: '%A2LvseOYKDXyuSGlXl3Sz0F5j2khVCN6JTf8ORD/tM8=.sha256',
+        name: 'a msg',
+      }
+    ], 'msg link')
+
+  t.deepEquals(mentions(
+    '[a blob](&9SSTQys34p9f4zqjxvRwENjFX0JapgtesRey7+fxK14=.sha256)'), [
+      {
+        link: '&9SSTQys34p9f4zqjxvRwENjFX0JapgtesRey7+fxK14=.sha256',
+        name: 'a blob',
+      }
+    ], 'blob link')
+
   t.end()
 })
 
-var refMention = {
-  feed: [ { link: id.feed, name: undefined } ],
-  msg: [ { link: id.msg, name: undefined } ],
-  blob: [ { link: id.blob, name: undefined } ]
-}
-refMention.all = refMention.feed.concat(refMention.msg, refMention.blob)
-
 test('ref mentions are detected', function (t) {
-  t.deepEquals(mentions(id.feed), refMention.feed, 'feed link')
-  t.deepEquals(mentions(id.blob), refMention.blob, 'blob link')
-  t.deepEquals(mentions(id.msg), refMention.msg, 'msg link')
-  t.deepEquals(mentions(id.all), refMention.all, 'all link')
+  t.deepEquals(mentions(
+    '@3HO6R2i60XNR3h6XCHAWCdt1k9Dwy+gaa2rVs6LzZ6Y=.ed25519'), [
+      {
+        link: '@3HO6R2i60XNR3h6XCHAWCdt1k9Dwy+gaa2rVs6LzZ6Y=.ed25519',
+        name: undefined
+      }
+    ], 'feed link')
+
+  t.deepEquals(mentions(
+    '%A2LvseOYKDXyuSGlXl3Sz0F5j2khVCN6JTf8ORD/tM8=.sha256'), [
+      {
+        link: '%A2LvseOYKDXyuSGlXl3Sz0F5j2khVCN6JTf8ORD/tM8=.sha256',
+        name: undefined
+      }
+    ], 'msg link')
+
+  t.deepEquals(mentions(
+    '&9SSTQys34p9f4zqjxvRwENjFX0JapgtesRey7+fxK14=.sha256'), [
+      {
+        link: '&9SSTQys34p9f4zqjxvRwENjFX0JapgtesRey7+fxK14=.sha256',
+        name: undefined
+      }
+    ], 'blob link')
+
   t.end()
 })
